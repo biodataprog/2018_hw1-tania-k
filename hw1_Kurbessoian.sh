5dd5fcb2c78ad015a1f7bbf37644ea5a5494a1e7
Tania Kurbessoian <tkurb001@ucr.edu>
2018-10-05 15:22:21 -0700

    Update hw1_Kurbessoian.sh

--- hw1_Kurbessoian.sh
+++ hw1_Kurbessoian.sh
@@ -14,7 +14,7 @@ du -h Ecoli-vs-Yersinia.BLASTP.tab
 
 head -25 Ecoli-vs-Yersinia.BLASTP.tab  
 tail -3 Ecoli-vs-Yersinia.BLASTP.tab
-echo wc -l
+echo wc -l Ecoli-vs-Yersinia.BLASTP.tab
 
 #4. Sorting
 
